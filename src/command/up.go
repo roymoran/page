c8010030d90fea974d9dbab04dce253c5baaa9ed
Roy Moran <romoran1@outlook.com>
2021-01-16 12:38:19 -0600

    add notes for logic needed to execute up command

--- src/command/up.go
+++ src/command/up.go
@@ -16,6 +16,23 @@ func (u Up) UsageCategory() int {
 }
 
 func (u Up) Execute() bool {
+	// Parse yaml file
+
+	// Resolve template url, is it valid?
+	// Download template from url, build static assets as needed,
+	// then read build files into memory. Take into consideration the size
+	// of the built assets - will it be ok to store in memory until deploy?
+	// or maybe copy these one by one into a deploy directory? maintaining a
+	// flag that signals deploy step once assets are ready.
+
+	// Get default host for host_value on yaml file. Does infrastructure
+	// exist to deploy assets? If not create infrastructure with message
+	// 'Creating infrastructure on [host_value]...'
+
+	// Get default registrar for registrar_value on yaml file,
+	// does domain exist on registrar? if not register with message
+	// 'Registering domain.com with [registrar_value]...'
+
 	return true
 }
 
