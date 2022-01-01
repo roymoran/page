4d7691fa58a1f435196d41ecf05d8b87d4cef9ab
Roy Moran <romoran1@outlook.com>
2022-01-01 16:36:18 -0600

    bump version v0.1.0-alpha.10. no change

--- src/constants/constants.go
+++ src/constants/constants.go
@@ -10,7 +10,7 @@ type Consts struct {
 
 var consts Consts = Consts{
 	appName:       "page cli",
-	appVersion:    "v0.1.0-alpha.8",
+	appVersion:    "v0.1.0-alpha.10",
 	acmeServerURL: "https://acme-v02.api.letsencrypt.org/directory",
 	analyticsID:   "UA-189047059-2",
 	appAuthors:    []string{"Roy Moran (https://github.com/roymoran)"},
